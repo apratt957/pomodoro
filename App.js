e8f92671363bf4b24b27836a61a0c22270849d9b
Andrew Pratt <apratt957@gmail.com>
2020-04-14 14:41:18 -0500

    added reset

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Timer from './components/Timer';
 import moment from 'moment';
 
 export default function App() {
-  const [sessionVal, setSessionVal] = useState(25);
+  const [sessionVal, setSessionVal] = useState(0.1);
   const [breakVal, setBreakVal] = useState(5);
   const [time, setTime] = useState(sessionVal * 60 * 1000);
   const [active, setActive] = useState(false);
@@ -24,13 +24,17 @@ export default function App() {
 
   const activeSwitch = () => setActive(!active);
 
+  const resetTime = () =>
+    working ? setTime(sessionVal * 60 * 1000) : setTime(breakVal * 60 * 1000);
+
   return (
     <View style={styles.container}>
       <Timer time={time} />
       <Button
-        title={active ? 'Stop' : 'Start'}
+        title={active ? 'Pause' : 'Start'}
         onPress={() => activeSwitch()}
       />
+      <Button title="Reset" onPress={() => resetTime()} />
     </View>
   );
 }
